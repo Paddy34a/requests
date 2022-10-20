2ff55da83bcf4ff56f0cc0e461a89a292840b24b
Paddy34a <luca.rottmann92@gmail.com>
2022-10-20 19:29:47 +0200

    - added application icon - started working on nav menu - added config initializer if not already present on local system (not sure when it's gonna be needed, but having a config folder is always good)

--- src/index.js
+++ src/index.js
@@ -1,37 +1,76 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
+const fs = require("fs"); 
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-// eslint-disable-next-line global-require
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.setIcon(path.join(__dirname, "images/main_icon.png"));  
+  mainWindow.setMenu(Menu.buildFromTemplate([
+    {
+      label: "File", 
+      submenu: [
+        {
+          label: "Open Config Folder", 
+          click: () => { require("child_process").exec(`start "" "${path.join(app.getPath("userData"), "config")}"`); }
+        },
+        {
+          label: "Quit", 
+          role: "close"
+        }
+      ]
+    }, 
+    {
+      label: "Developer", 
+      submenu: [
+        {
+          label: "Toggle Developer Tools", 
+          role: "toggleDevTools", 
+        },
+      ]
+    }
+  ])); 
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  const appPath = app.getPath("userData"); 
+  try {
+
+    const configDir = path.join(appPath, "config"); 
+
+    if(fs.existsSync(configDir)) {
+
+      console.log("[requests] found config directory"); 
+
+    } else {
+
+      console.log("[requests] could not find config directory, initializing new config directory..."); 
+      fs.mkdirSync(configDir); 
+      console.log("[requests] successfully created dir"); 
+
+    }
+    
+  } catch (e) {
+    console.error("[requests] an unknown error occured whilst trying to fetch directories."); 
+  }
+
+  //mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow(); 
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -39,12 +78,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
